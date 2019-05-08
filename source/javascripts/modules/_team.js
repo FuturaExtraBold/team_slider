5320904423be31ac90c63868d1d7604ac7352915
Ben Hays <ben@kajabi.com>
2019-05-08 11:06:26 -0700

    Initial commit.

---
+++ source/javascripts/modules/_team.js
@@ -0,0 +1,17 @@
+(function($) {
+
+  var $slide = $(".slide");
+  console.log("slide length:", $slide.eq(0).outerWidth());
+
+  $(".team__slider").css({
+    "width": $slide.length * $slide.eq(0).outerWidth(),
+    "opacity": 1
+  });
+
+  Draggable.create(".team__slider", {
+    bounds: $(".team__container"),
+    throwProps: true,
+    type: "x",
+  });
+
+})(jQuery);
