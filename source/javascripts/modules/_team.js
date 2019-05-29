d0dfd95e5eb3f5fe35ed0b0c8976ff6bb9ba9450
Ben Hays <ben@kajabi.com>
2019-05-29 11:12:15 -0700

    Fixed slider intitial state, fade in.

--- source/javascripts/modules/_team.js
+++ source/javascripts/modules/_team.js
@@ -1,12 +1,27 @@
 (function($) {
 
   var $slide = $(".slide");
-  console.log("slide length:", $slide.eq(0).outerWidth());
+  var $slider = $(".team__slider");
+  var $image2, $image3;
 
-  $(".team__slider").css({
-    "width": $slide.length * $slide.eq(0).outerWidth(),
-    "opacity": 1
-  });
+  // Slider
+  TweenMax.set($slider, { width: $slide.length * $slide.eq(0).outerWidth(), opacity: 0 });
+  TweenMax.to($slider, 0.5, { opacity: 1 });
+
+  function handleSlideMouseover(event) {
+    $image2 = $(event.currentTarget).find(".slide__image--2");
+    $image3 = $(event.currentTarget).find(".slide__image--3");
+    TweenMax.to($image3, 0.2, { opacity: 1 });
+  }
+
+  function handleSlideMouseout(event) {
+    $image2 = $(event.currentTarget).find(".slide__image--2");
+    $image3 = $(event.currentTarget).find(".slide__image--3");
+    TweenMax.set([$image2, $image3], { opacity: 0 });
+  }
+
+  $slide.on("mouseover", handleSlideMouseover);
+  $slide.on("mouseout", handleSlideMouseout);
 
   Draggable.create(".team__slider", {
     bounds: $(".team__slider-container"),
